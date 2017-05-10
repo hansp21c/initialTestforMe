5a28b0eafa76101833d96156de4e53a8a372f206
apple <hansp21c@gmail.com>
2017-05-10 13:26:10 -0500

    Refactored

--- app/src/main/java/com/example/apple/prontoshop/MainActivity.java
+++ app/src/main/java/com/example/apple/prontoshop/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
             System.out.println(i);
         }
         // someone else fixes all my bugs.
+        // test from android;
     }
 
     @Override
